fc9bde1776dad684f9c0fc3775dbe3b740d233a6
imelregistrasi <imelregistrasi@gmail.com>
2025-01-16 16:16:29 +0700

    Update api.php
    test 9

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('test9', function (Request $request){
+    return response()->json(['message' => 'API route accessed successfully9']);
+});
+
 Route::get('test8', function (Request $request){
     return response()->json(['message' => 'API route accessed successfully8']);
 });
